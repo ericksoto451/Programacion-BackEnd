def07c64b95490057ee5c04dad5fd6640c0eaa10
ericksoto451 <erick_soto@dtidimensional.mx>
2023-02-23 19:22:40 -0600

    Create desafio_1.js
    Se sube el desafio 1

---
+++ desafio_1.js
@@ -0,0 +1,68 @@
+
+class ProductManager {
+    constructor(products){
+        this.products = products;
+    }
+
+
+    addProduct(title, description, price, thumbnail,stock){
+        let idCreated=this.products.length + 1;
+        let product=[
+            {
+                title: title,
+                description: description,
+                price: price,
+                thumbnail: thumbnail,
+                code: idCreated,
+                stock: stock
+            }
+        ];
+        
+        this.products.push(product);
+
+    }
+
+
+    getProducts(){
+        let result;
+        if(this.products.length === 0){
+            result= "No hay productos";
+        }else{
+            result= this.products;
+        }
+        return result;
+    }
+
+
+    getProductsById(id){
+        let result;
+        this.products.forEach(objeto => {
+            Object.values(objeto).forEach(item =>{
+                const { code } = item;
+                if(id === code)result=[...objeto]
+                
+            })
+        })
+        if(result=== undefined) result="NOT FOUND";
+        return result;
+    }
+
+
+}
+
+let products=[];
+const productManager1 = new ProductManager(products);
+console.log(productManager1.getProducts());
+productManager1.addProduct("Jetta 2019", 
+                           "Jetta 2019 confortline con quemacocos",
+                           250000,
+                           "https://d2n4wb9orp1vta.cloudfront.net/cms/brand/PT-Mex/2019-PT-Mex/jetta.jpg;maxWidth=1200",
+                           1);
+
+productManager1.addProduct("CRV 2019", 
+                           "CRV 2019 con asientos de piel",
+                           250000,
+                           "https://parentesis.com/imagesPosts/CR-V-2019-2.jpg",
+                           3);
+console.log(productManager1.getProducts());
+console.log(productManager1.getProductsById(6));
